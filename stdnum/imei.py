6bb04af37fb1dbcb719055eaaff23f7dbe3f1246
Arthur de Jong <arthur@arthurdejong.org>
2010-08-20 14:16:55 +0000

    add an IMEI (International Mobile Equipment Identity) module
    git-svn-id: http://arthurdejong.org/svn/python-stdnum/python-stdnum@26 9dea7c4f-944c-4273-ac1a-574ede026edc

---
+++ stdnum/imei.py
@@ -0,0 +1,69 @@
+# imei.py - functions for handling International Mobile Equipment Identity
+#           (IMEI) numbers
+#
+# Copyright (C) 2010 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""Module for handling IMEI (International Mobile Equipment Identity)
+numbers, used to identify mobile phones.
+
+>>> is_valid('35686800-004141-20')
+True
+>>> is_valid('35-417803-685978-1') # incorrect check digit
+False
+>>> compact('35686800-004141-20')
+'3568680000414120'
+>>> format('354178036859789')
+'35-417803-685978-9'
+>>> imei_type('35686800-004141-20')
+'IMEISV'
+"""
+
+
+def compact(number):
+    """Convert the IMEI number to the minimal representation. This strips the
+    number of any valid separators and removes surrounding whitespace."""
+    number = number.replace(' ','').replace('-','').strip().upper()
+    return number
+
+def imei_type(number):
+    """Check the passed number and returns 'IMEI', 'IMEISV' or None (for
+    invalid) for checking the type of number passed."""
+    try:
+        number = compact(number)
+    except:
+        return None
+    if len(number) == 14: # IMEI without check digit
+        return 'IMEI' if number.isdigit() else None
+    if len(number) == 15: # IMEI with check digit
+        from stdnum import luhn
+        return 'IMEI' if luhn.is_valid(number) else None
+    elif len(number) == 16:
+        return 'IMEISV' if number.isdigit() else None
+    else:
+        return None
+
+def is_valid(number):
+    """Checks to see if the number provided is a valid IMEI (or IMEISV)
+    number."""
+    return imei_type(number) is not None
+
+def format(number, separator='-'):
+    """Reformat the passed number to the standard format."""
+    number = compact(number)
+    number = ( number[:2], number[2:8], number[8:14], number[14:] )
+    return separator.join(x for x in number if x)
