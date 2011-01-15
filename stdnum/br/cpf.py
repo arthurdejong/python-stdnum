fda67ac2e53f2de0766c590cee06b2189ea58b54
Arthur de Jong <arthur@arthurdejong.org>
2011-01-15 20:26:53 +0000

    add a CPF (Cadastro de Pessoas Físicas) module
    git-svn-id: http://arthurdejong.org/svn/python-stdnum/python-stdnum@45 9dea7c4f-944c-4273-ac1a-574ede026edc

---
+++ stdnum/br/cpf.py
@@ -0,0 +1,65 @@
+# cpf.py - functions for handling CPF numbers
+# coding: utf-8
+#
+# Copyright (C) 2011 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""Module for handling CPF (Cadastro de Pessoas Físicas) numbers, the
+Brazillian national identification number.
+
+>>> is_valid('390.533.447-05')
+True
+>>> is_valid('231.002.999-00')
+False
+>>> compact('390.533.447-05')
+'39053344705'
+>>> format('23100299900')
+'231.002.999-00'
+"""
+
+
+def compact(number):
+    """Convert the number to the minimal representation. This strips the
+    number of any valid separators and removes surrounding whitespace."""
+    number = number.replace(' ','').replace('-','').replace('.','').strip()
+    # pad with leading zeroes
+    return number
+
+def _calc_check_digits(number):
+    """Calculate the check digits for the number."""
+    d1 = sum( (10-i) * int(number[i]) for i in range(9) )
+    d1 = ( 11 - d1 ) % 11 % 10
+    d2 = sum( (11-i) * int(number[i]) for i in range(9) ) + 2 * d1
+    d2 = ( 11 - d2 ) % 11 % 10
+    return '%d%d' % (d1, d2)
+
+def is_valid(number):
+    """Checks to see if the number provided is a valid BSN. This checks
+    the length and whether the check digit is correct."""
+    try:
+        number = compact(number)
+    except:
+        return False
+    return len(number) == 11 and \
+           number.isdigit() and \
+           int(number) > 0 and \
+           _calc_check_digits(number) == number[-2:]
+
+def format(number):
+    """Reformat the passed number to the standard format."""
+    number = compact(number)
+    return number[:3] + '.' + number[3:6] + '.' + number[6:-2] + '-' + number[-2:]
