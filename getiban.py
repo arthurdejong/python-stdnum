db2238cee095f34f78fbab764d7f2010dba29ac6
Arthur de Jong <arthur@arthurdejong.org>
2011-01-16 21:48:12 +0000

    add an IBAN (International Bank Account Number) module
    git-svn-id: http://arthurdejong.org/svn/python-stdnum/python-stdnum@51 9dea7c4f-944c-4273-ac1a-574ede026edc

---
+++ getiban.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+# getiban.py - script to donwload and parse data from the IBAN registry
+#
+# Copyright (C) 2011 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""This script downloads data from SWIFT (the Society for Worldwide Interbank
+Financial Telecommunication which is the official IBAN registrar) to get
+the data needed to correctly parse and validate IBANs."""
+
+import urllib
+import re
+
+
+# The place where the current version of IBAN_Registry.txt can be downloaded.
+download_url = 'http://www.swift.com/dsp/resources/documents/IBAN_Registry.txt'
+
+def splitlines(f):
+    """Read lines from the TAB-delimited IBAN_Registry.txt file and return
+    a dictionary per read line. We clean up the values a bit because it
+    contains some junk."""
+    stripit = ' \t\n\r;:\'"'
+    firstline = [ x.strip(stripit) for x in f.readline().lower().split('\t') ]
+    for line in f:
+        yield dict(zip(firstline, [ x.strip(stripit) for x in line.split('\t') ]))
+
+def get_country_codes(line):
+    """Return the list of country codes this line has."""
+    # simplest case first
+    if len(line['country code as defined in iso 3166']) == 2:
+        return [ line['country code as defined in iso 3166'] ]
+    # fall back to parsing the IBAN structure
+    return [ x.strip()[:2] for x in line['iban structure'].split(',') ]
+
+def parse(f):
+    """Parse the specified file."""
+    print '# generated from IBAN_Registry.txt, downloaded from'
+    print '# %s' % download_url
+    for line in splitlines(f):
+        for cc in get_country_codes(line):
+            # print country line
+            print '%s country="%s" bban="%s"' % ( cc, line['name of country'], line['bban structure'])
+            # TODO: some countries have a fixed check digit value
+            # TODO: some countries have extra check digits
+            # TODO: use "Bank identifier position within the BBAN" field
+            #       to add labels to the ranges (Bank identifier and Branch Identifier)
+
+if __name__ == '__main__':
+    #f = open('IBAN_Registry.txt', 'r')
+    f = urllib.urlopen(download_url)
+    parse(f)
---
+++ stdnum/iban.py
@@ -0,0 +1,91 @@
+# iban.py - functions for handling International Bank Account Numbers (IBANs)
+#
+# Copyright (C) 2011 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""Module for handling IBANs (International Bank Account Numbers)
+numbers, used to identify bank accounts across national borders.
+
+
+>>> is_valid('GR16 0110 1050 0000 1054 7023 795')
+True
+>>> is_valid('BE31435411161155')
+True
+>>> compact('GR16 0110 1050 0000 1054 7023 795')
+'GR1601101050000010547023795'
+>>> format('GR1601101050000010547023795')
+'GR16 0110 1050 0000 1054 7023 795'
+"""
+
+from stdnum import numdb
+from stdnum.iso7064 import mod_97_10
+import re
+
+
+# our open copy of the IBAN database
+_ibandb = numdb.get('iban')
+
+def compact(number):
+    """Convert the iban number to the minimal representation. This strips the
+    number of any valid separators and removes surrounding whitespace."""
+    return number.replace(' ','').replace('-','').strip().upper()
+
+_alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def _convert(number):
+    """Prepare the number to it's base10 representation (also moving the
+    check digits to the end) so it can be checked with the ISO 7064
+    Mod 97, 10 algorithm."""
+    # TODO: find out whether this should be in the mod_97_10 module
+    return ''.join(str(_alphabet.index(x)) for x in number[4:] + number[:4])
+
+# regular expression to check IBAN structure
+_struct_re = re.compile('([1-9][0-9]*)!([nac])')
+
+def _matches_structure(number, structure):
+    """Check the supplied number against the supplied structure."""
+    start = 0
+    for length, code in _struct_re.findall(structure):
+        length = int(length)
+        if code == 'n' and not number[start:start + length].isdigit():
+            return False
+        elif code == 'a' and not number[start:start + length].isalpha():
+            return False
+        elif code == 'c' and not number[start:start + length].isalnum():
+            return False # should not happen due to checksum check
+        start += length
+    # the whole number should be parsed now
+    return start == len(number)
+
+def is_valid(number):
+    """Checks to see if the number provided is a valid IBAN."""
+    try:
+        number = compact(number)
+    except:
+        return False
+    # ensure that checksum is valid
+    if not mod_97_10.is_valid(_convert(number)):
+        return False
+    # look up the number
+    info = _ibandb.info(number)
+    # check if the number has the correct structure
+    return _matches_structure(number[4:], info[0][1].get('bban', ''))
+
+def format(number, separator=' '):
+    """Reformat the passed number to the space-separated format."""
+    number = compact(number)
+    return separator.join(number[i:i+4] for i in range(0,len(number),4))
