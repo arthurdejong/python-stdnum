e517903e7279f1240d7a54c1d87d2037cbe3ec56
Arthur de Jong <arthur@arthurdejong.org>
2011-02-05 22:43:44 +0000

    add an ISIL (International Standard Identifier for Libraries and Related Organizations) module
    git-svn-id: http://arthurdejong.org/svn/python-stdnum/python-stdnum@61 9dea7c4f-944c-4273-ac1a-574ede026edc

---
+++ getisil.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+# getisil.py - script to donwload ISIL agencies
+#
+# Copyright (C) 2011 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""This script downloads a web page from the ISIL Registration Authority
+and screen-scrapes the national and non-national ISIL agencies and
+code prefixes."""
+
+import urllib
+import BeautifulSoup
+import re
+
+spaces_re = re.compile('\s+', re.UNICODE)
+
+# the web page that holds information on the ISIL authorities
+download_url = 'http://biblstandard.dk/isil/'
+
+def clean(s):
+    """Cleans up the string removing unneeded stuff from it."""
+    return spaces_re.sub(' ', s.replace(u'\u0096', '')).strip().encode('utf-8')
+
+def parse(f):
+    """Parse the specified file."""
+    print '# generated from ISIL Registration Authority, downloaded from'
+    print '# %s' % download_url
+    soup = BeautifulSoup.BeautifulSoup(f, convertEntities='html')
+    # find all table rows
+    for tr in soup.findAll('tr'):
+        # find the rows with four columns of text
+        tds = tr.findAll('td', attrs={'class': 'text'}, recursive=False)
+        if len(tds) == 4:
+            props = {}
+            cc = clean(tds[0].string)
+            if tds[1].string:
+                props['country'] = clean(tds[1].contents[0])
+            ra_a = tds[2].find('a')
+            if ra_a:
+                props['ra'] = clean(ra_a.string)
+                props['ra_url'] = clean(ra_a['href'])
+            elif tds[2].string:
+                props['ra'] = clean(tds[2].string)
+            # we could also get the search urls from tds[3].findAll('a')
+            print '%s$ %s' % ( cc,
+                  ' '.join([ '%s="%s"' % (x, y) for x, y in props.iteritems() ]) )
+
+if __name__ == '__main__':
+    #f = open('isil.html', 'r')
+    f = urllib.urlopen(download_url)
+    parse(f)
---
+++ stdnum/isil.py
@@ -0,0 +1,68 @@
+# isil.py - functions for handling identifiers for libraries and related
+#           organizations
+#
+# Copyright (C) 2011 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""Module for handling ISIL (International Standard Identifier for
+Libraries and Related Organizations).
+
+>>> is_valid('IT-RM0267')
+True
+>>> is_valid('OCLC-DLC')
+True
+>>> is_valid('WW-RM0267') # unregistered country code
+False
+>>> format('it-RM0267')
+'IT-RM0267'
+"""
+
+# the valid characters in an ISIL
+_alphabet = set('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-:/')
+
+def compact(number):
+    """Convert the ISIL to the minimal representation. This strips
+    surrounding whitespace."""
+    return number.strip()
+
+def _known_agency(agency):
+    """Checks whether the specified agency is valid."""
+    # look it up in the db
+    from stdnum import numdb
+    results = numdb.get('isil').info(agency.upper() + '$')
+    # there should be only one part and it should have properties
+    return len(results) == 1 and bool(results[0][1])
+
+def is_valid(number):
+    """Checks to see if the number provided is a valid isil (or isilSV)
+    number."""
+    try:
+        number = compact(number)
+    except:
+        return False
+    for n in number:
+        if n not in _alphabet:
+            return False
+    return len(number) <= 15 and _known_agency(number.split('-')[0])
+
+def format(number):
+    """Reformat the passed number to the standard format."""
+    number = compact(number)
+    parts = number.split('-')
+    if len(parts) > 1 and _known_agency(parts[0]):
+        parts[0] = parts[0].upper()
+    return '-'.join(parts)
