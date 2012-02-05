9f1d47b3ca49227fa5316066f264ab61cccdb03c
Arthur de Jong <arthur@arthurdejong.org>
2012-02-05 21:12:18 +0000

    add a SIREN (Système d'Identification du Répertoire des Entreprises, a French company identification number) module
    git-svn-id: http://arthurdejong.org/svn/python-stdnum/python-stdnum@102 9dea7c4f-944c-4273-ac1a-574ede026edc

---
+++ stdnum/fr/siren.py
@@ -0,0 +1,49 @@
+# siren.py - functions for handling French SIREN numbers
+# coding: utf-8
+#
+# Copyright (C) 2012 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""Module for handling French SIREN (Système d’Identification du
+Répertoire des Entreprises) numbers, used to identify French companies.
+
+>>> compact('552 008 443')
+'552008443'
+>>> is_valid('404833048')
+True
+>>> is_valid('404833047')
+False
+"""
+
+from stdnum.util import clean
+from stdnum import luhn
+
+
+def compact(number):
+    """Convert the number to the minimal representation. This strips the
+    number of any valid separators and removes surrounding whitespace."""
+    return clean(number, ' ').strip()
+
+
+def is_valid(number):
+    """Checks to see if the number provided is a valid number. This checks
+    the length, formatting and check digit."""
+    try:
+        number = compact(number)
+    except:
+        return False
+    return len(number) == 9 and number.isdigit() and luhn.is_valid(number)
