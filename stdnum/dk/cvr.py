74f4e2a2db411a99edf33b918df0afd201464160
Arthur de Jong <arthur@arthurdejong.org>
2012-02-11 20:26:46 +0000

    add a CVR (Momsregistreringsnummer, Danish VAT number) module
    git-svn-id: http://arthurdejong.org/svn/python-stdnum/python-stdnum@119 9dea7c4f-944c-4273-ac1a-574ede026edc

---
+++ stdnum/dk/cvr.py
@@ -0,0 +1,59 @@
+# cvr.py - functions for handling Danish CVR numbers
+#
+# Copyright (C) 2012 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+
+"""Module for handling Danish CVR (Momsregistreringsnummer, VAT) numbers.
+
+It is an 8 digit number with a straightforward check mechanism.
+
+>>> compact('DK 13585628')
+'13585628'
+>>> is_valid('DK 13585628')
+True
+>>> is_valid('DK 13585627')
+False
+"""
+
+from stdnum.util import clean
+
+
+def compact(number):
+    """Convert the number to the minimal representation. This strips the
+    number of any valid separators and removes surrounding whitespace."""
+    number = clean(number, ' -.,/:').upper().strip()
+    if number.startswith('DK'):
+        number = number[2:]
+    return number
+
+
+def checksum(number):
+    """Calculate the checksum."""
+    weights = (2, 7, 6, 5, 4, 3, 2, 1)
+    return sum(weights[i] * int(n) for i, n in enumerate(number)) % 11
+
+
+def is_valid(number):
+    """Checks to see if the number provided is a valid VAT number. This checks
+    the length, formatting and check digit."""
+    try:
+        number = compact(number)
+    except:
+        return False
+    return len(number) == 8 and number.isdigit() and \
+           number[0] != '0' and checksum(number) == 0
