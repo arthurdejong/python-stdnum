20296ef3e03aba705ac7d75528fc298d44cf43e0
Arthur de Jong <arthur@arthurdejong.org>
2012-02-17 16:15:53 +0000

    add a KMKR (Käibemaksukohuslase, Estonian VAT number) module
    git-svn-id: http://arthurdejong.org/svn/python-stdnum/python-stdnum@135 9dea7c4f-944c-4273-ac1a-574ede026edc

---
+++ stdnum/ee/kmkr.py
@@ -0,0 +1,56 @@
+# kmkr.py - functions for handling Estonian VAT numbers
+# coding: utf-8
+#
+# Copyright (C) 2012 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""Module for handling Estonian KMKR (Käibemaksukohuslase, VAT) numbers.
+
+>>> compact('EE 100 931 558')
+'100931558'
+>>> is_valid('100594102')
+True
+>>> is_valid('100594103')  # incorrect check digit
+False
+"""
+
+from stdnum.util import clean
+
+
+def compact(number):
+    """Convert the number to the minimal representation. This strips the
+    number of any valid separators and removes surrounding whitespace."""
+    number = clean(number, ' ').upper().strip()
+    if number.startswith('EE'):
+        number = number[2:]
+    return number
+
+
+def checksum(number):
+    """Calculate the checksum."""
+    weights = (3, 7, 1, 3, 7, 1, 3, 7, 1)
+    return sum(weights[i] * int(n) for i, n in enumerate(number)) % 10
+
+
+def is_valid(number):
+    """Checks to see if the number provided is a valid VAT number. This
+    checks the length, formatting and check digit."""
+    try:
+        number = compact(number)
+    except:
+        return False
+    return number.isdigit() and len(number) == 9 and checksum(number) == 0
