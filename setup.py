907e41082fe4d05556d402dc30376fce9f32d344
Arthur de Jong <arthur@arthurdejong.org>
2012-02-25 21:06:09 +0000

    add the optional dependency on suds for the stdnum.eu.vat.check_vies() function
    git-svn-id: http://arthurdejong.org/svn/python-stdnum/python-stdnum@164 9dea7c4f-944c-4273-ac1a-574ede026edc

--- setup.py
+++ setup.py
@@ -52,4 +52,8 @@ setup(name='python-stdnum',
           ],
       packages=find_packages(),
       package_data={'stdnum': ['*.dat']},
+      install_requires=['distribute'],
+      extras_require={
+          'VIES':  ['suds'],
+          },
       )
