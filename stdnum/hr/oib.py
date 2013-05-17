31f268433bfec7d01ae8071cb0c37084e455d150
Arthur de Jong <arthur@arthurdejong.org>
2013-05-17 13:26:37 +0200

    Implement validate() for Croatian numbers

--- stdnum/hr/oib.py
+++ stdnum/hr/oib.py
@@ -1,7 +1,7 @@
 # cnp.py - functions for handling Croatian OIB numbers
 # coding: utf-8
 #
-# Copyright (C) 2012 Arthur de Jong
+# Copyright (C) 2012, 2013 Arthur de Jong
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -24,16 +24,17 @@ The personal identification number is used to identify persons and legal
 entities in Croatia. It has 11 digits (sometimes prefixed by HR), contains
 no personal information and uses the ISO 7064 Mod 11, 10 checksum algorithm.
 
->>> compact('HR 33392005961')
+>>> validate('HR 33392005961')
 '33392005961'
->>> is_valid('33392005961')
-True
->>> is_valid('33392005962')  # invalid check digit
-False
+>>> validate('33392005962')  # invalid check digit
+Traceback (most recent call last):
+    ...
+InvalidChecksum: ...
 """
 
-from stdnum.util import clean
+from stdnum.exceptions import *
 from stdnum.iso7064 import mod_11_10
+from stdnum.util import clean
 
 
 def compact(number):
@@ -45,12 +46,22 @@ def compact(number):
     return number
 
 
+def validate(number):
+    """Checks to see if the number provided is a valid OIB number. This
+    checks the length, formatting and check digit."""
+    number = compact(number)
+    if not number.isdigit():
+        raise InvalidFormat()
+    if len(number) != 11:
+        raise InvalidLength()
+    mod_11_10.validate(number)
+    return number
+
+
 def is_valid(number):
     """Checks to see if the number provided is a valid OIB number. This
     checks the length, formatting and check digit."""
     try:
-        number = compact(number)
-    except:
+        return bool(validate(number))
+    except ValidationError:
         return False
-    return len(number) == 11 and number.isdigit() and \
-           mod_11_10.is_valid(number)
