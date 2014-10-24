dd309e4e2279a1c648546f9417c3f624ccbd6f21
Tomas Thor Jonsson <benregn@gmail.com>
2014-10-24 13:25:30 +0200

    Add support for SE orgnr
    This also delegates some of the validation for the Swedish VAT module to
    the orgnr module.

--- stdnum/se/orgnr.py
+++ stdnum/se/orgnr.py
@@ -1,7 +1,8 @@
-# vat.py - functions for handling Swedish VAT numbers
+# orgnr.py - functions for handling Swedish organisation numbers
 # coding: utf-8
 #
-# Copyright (C) 2012, 2013 Arthur de Jong
+# Copyright (C) 2012-2015 Arthur de Jong
+# Copyright (C) 2014 Tomas Thor Jonsson
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -18,48 +19,55 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
-"""VAT (Moms, Mervärdesskatt, Swedish VAT number).
+"""Orgnr (Organisationsnummer, Swedish company number).
 
-The Momsregistreringsnummer is used for VAT (Moms, Mervärdesskatt) purposes
-and consists of 12 digits of which the last two should be 01. The first 10
-digits should have a valid Luhn checksum.
+The Orgnr (Organisationsnummer) is the national number to identify Swedish
+companies and consists of 10 digits. These are the first 10 digits in the
+Swedish VAT number, i.e. it's the VAT number without the 'SE' in front and
+the '01' at the end.
 
->>> validate('SE 123456789701')
-'123456789701'
->>> validate('123456789101')  # invalid check digits
+>>> validate('1234567897')
+'1234567897'
+>>> validate('1234567891')  # invalid check digits
 Traceback (most recent call last):
     ...
 InvalidChecksum: ...
+>>> format('123456-7897')
+'123456-7897'
 """
 
+from stdnum import luhn
 from stdnum.exceptions import *
-from stdnum.se import orgnr
 from stdnum.util import clean
 
 
 def compact(number):
     """Convert the number to the minimal representation. This strips the
     number of any valid separators and removes surrounding whitespace."""
-    number = clean(number, ' -.').upper().strip()
-    if number.startswith('SE'):
-        number = number[2:]
-    return number
+    return clean(number, ' -.').strip()
 
 
 def validate(number):
-    """Checks to see if the number provided is a valid VAT number. This
-    checks the length, formatting and check digit."""
+    """Checks to see if the number provided is a valid number. This checks
+    the length, formatting and check digit."""
     number = compact(number)
-    if not number.isdigit() or number[-2:] != '01':
+    if not number.isdigit():
         raise InvalidFormat()
-    orgnr.validate(number[:-2])
-    return number
+    if len(number) != 10:
+        raise InvalidLength()
+    return luhn.validate(number)
 
 
 def is_valid(number):
-    """Checks to see if the number provided is a valid VAT number. This
-    checks the length, formatting and check digit."""
+    """Checks to see if the number provided is a valid number. This checks
+    the length, formatting and check digit."""
     try:
         return bool(validate(number))
     except ValidationError:
         return False
+
+
+def format(number):
+    """Reformat the number to the standard format."""
+    number = compact(number)
+    return number[:6] + '-' + number[6:]
