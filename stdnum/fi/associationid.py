db2474654d20f331585b60074ec0a87b755010d0
Arthur de Jong <arthur@arthurdejong.org>
2015-07-12 15:31:43 +0200

    Add a whitelist for short numbers and fix API
    This implements separate functions compact() and format() and fixes the
    doctests. This also implements a whitelist of registered short numbers
    to avoid accidentally validating just any number.

--- stdnum/fi/associationid.py
+++ stdnum/fi/associationid.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 #
 # Copyright (C) 2015 Holvi Payment Services Oy
+# Copyright (C) 2015 Arthur de Jong
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -18,32 +19,44 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
-""" Finnish Association Identifier
+"""Finnish Association Identifier.
 
 The number consists of 1 to 6 digits that are normally separated with a dot
-in groups of 0-3 and 0-3 numbers. E.g. 123.123, 12.123, 1.123, 123 or 1
+in groups of 0-3 and 0-3 numbers. E.g. 123.123, 12.123, 1.123, 123 or 1.
 
->>> businessid.validate('123.123')
-u'123.123'
-
->>> businessid.validate('1123')
-u'1.123'
-
->>> businessid.validate('123123123')
+>>> validate('123.123')
+'123123'
+>>> validate('1123')
+'1123'
+>>> validate('123123123')
 Traceback (most recent call last):
   ...
 stdnum.exceptions.InvalidLength: The number has an invalid length.
-
->>> businessid.validate('12df')
+>>> validate('12df')
 Traceback (most recent call last):
   ...
 stdnum.exceptions.InvalidFormat: The number has an invalid format.
-
+>>> format('123')
+'123'
+>>> format('1234')
+'1.234'
 """
 
-import re
 from stdnum.exceptions import *
 from stdnum.util import clean
+
+
+# a collection of all registered numbers with 2 or less digits
+_lownumbers = set((
+    1, 6, 7, 9, 12, 14, 15, 16, 18, 22, 23, 24, 27, 28, 29, 35, 36, 38, 40,
+    41, 42, 43, 45, 46, 50, 52, 55, 58, 60, 64, 65, 68, 72, 75, 76, 77, 78,
+    83, 84, 85, 89, 92))
+
+
+def compact(number):
+    """Convert the number to the minimal representation. This strips the
+    number of any valid separators and removes surrounding whitespace."""
+    return clean(number, ' -._+').strip()
 
 
 def validate(number):
@@ -53,24 +66,29 @@ def validate(number):
     First strip all separators and spaces from the number and then checks
     that it has a correct length and is only numeric.
     """
-    number = clean(number, ' -._+').strip()
-
+    number = compact(number)
     if not number.isdigit():
         raise InvalidFormat()
-
     if len(number) < 1 or len(number) > 6:
         raise InvalidLength()
-
-    if len(number) < 4:
-        return number
-    else:
-        return "%s.%s" % (number[:-3], number[-3:])
+    if len(number) < 3 and int(number) not in _lownumbers:
+        raise InvalidComponent()
+    return number
 
 
 def is_valid(number):
-    """Checks to see if the number provided is a valid association register number.
-    This checks that the format is correct."""
+    """Checks to see if the number provided is a valid association register
+    number. This checks that the format is correct."""
     try:
         return bool(validate(number))
     except ValidationError:
         return False
+
+
+def format(number):
+    """Reformat the passed number to the standard format."""
+    number = compact(number)
+    if len(number) <= 3:
+        return number
+    else:
+        return number[:-3] + '.' + number[-3:]
