76c0667e5c9dea4bded58acf2d826f921d46e15c
Yoann Aubineau <y.aubineau@criteo.com>
2016-05-27 16:43:53 +0200

    add a SIRET (Système d'Identification du Répertoire des ETablissements, a French company's etablishment identification number) module

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'python-stdnum'
-copyright = u'2013, Arthur de Jong'
+copyright = u'2013, Arthur de Jong. 2016, Yoann Aubineau'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
