8878922676acc20bf437e54d4699687345269216
David García Garzón <david.garcia@upf.edu>
2016-08-26 18:19:38 +0200

    support for spanish 'Referencia Catastral'

---
+++ stdnum/es/referenciacatastral.py
@@ -0,0 +1,166 @@
+# referenciacatastral.py - functions for handling Spanish real state ids
+# coding: utf-8
+#
+# Copyright (C) 2011-2015 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""Referencia Catastral (Spanish real estate property id)
+
+The cadastral reference code is an id for real estates in Spain.
+It has 20 digits, being numbers and capital letters including spanish Ñ.
+First 14 digits identify the parcel, next 4 are a sequence number
+identifying different properties within the parcel, 
+and the last 2 are two checksum letters.
+
+The parcel is identified diferently in urban, non-urban or 
+special (infrastructure) states:
+
+* Urban states
+    * 7 digits: (numeric) block sequence within cartographic sheet,
+        and parcel sequence within the block.
+    * 7 digits: (alphanum) cartographic sheet where the block centroid is.
+
+* Non-urban states:
+    * 2 digits: (numeric) Province or more exactly, ministerial delegation
+    * 3 digits: (numeric) Municipality
+    * 1 digits: (letter) Sector
+    * 3 digits: (numeric) Polygon
+    * 5 digits: (numeric) Parcel
+
+* Special states:
+    * BICE code: (2 digits, a number and a letter)
+        * Energy infrastructure
+            * 1E: Thermal power plant
+            * 1G: Regasification terminal
+            * 1R: Oil refinery
+            * 1N: Nuclear power plant
+            * 1H: Hydro power plant
+        * Hydro
+            * 2P: Dam
+        * Roads and highways
+            * 3A: Tolled highway
+            * 3C: Tolled road
+            * 3T: Tolled tunnel
+        * Ports and airports
+            * 4A: Aeropuertos:
+            * 4P: Puertos comerciales:
+    * Province (major if many) (2 digits)
+    * Municipality (3 digits)
+    * Scope (1 letter, leter)
+        * E: National
+        * A: Autonomous Comunity
+        * P: Province
+        * M: Municipality
+    * Number of municipalities it extends (2 digits, numbers)
+    * Abreviated denomination (4 digits, letters)
+
+More information:
+
+* http://www.catastro.meh.es/documentos/05042010_P.pdf
+* http://trellat.es/validar-la-referencia-catastral-en-javascript/
+
+>>> compact('7837301-VG8173B-0001 TT')
+'7837301VG8173B0001TT'
+
+>>> format('7837301VG8173B0001TT') # Urban: Lanteira town hall
+'7837301 VG8173B 0001 TT'
+>>> format('4A08169P03PRAT0001LR') # Special: BCN Airport
+'4A08169 P03PRAT 0001 LR'
+
+>>> validate('7837301 VG8173B 0001 TT')
+'7837301VG8173B0001TT'
+>>> validate('783301 VG8173B 0001 TT')
+Traceback (most recent call last):
+    ...
+ValidationError: Wrong length for cadastral reference \
+'783301VG8173B0001TT', should be 20
+>>> validate('7837301/VG8173B 0001 TT')
+Traceback (most recent call last):
+    ...
+ValidationError: Found invalid digits '/'
+>>> validate('7837301 VG8173B 0001 NN')
+Traceback (most recent call last):
+    ...
+ValidationError: Control code should be TT instead of NN
+
+>>> is_valid('7837301VG8173B0001TT')
+True
+>>> is_valid('7837301VG8173B0001NN')
+False
+
+"""
+
+from stdnum.exceptions import *
+from stdnum.util import clean
+
+_valids = u'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789'
+
+def compact(reference):
+    return clean(reference, ' -').strip().upper()
+
+def format(reference):
+    reference = compact(reference)
+    return ' '.join([
+        reference[0:7],
+        reference[7:14],
+        reference[14:18],
+        reference[18:20]
+        ])
+
+def validate(reference):
+    reference = compact(reference)
+    invalidchars = ''.join(c for c in reference if c not in _valids)
+    if invalidchars:
+        raise ValidationError(
+            "Found invalid digits '{}'".format(invalidchars))
+
+    # TODO: rustic ones are 19?
+    if len(reference) != 20:
+        raise ValidationError(
+            "Wrong length for cadastral reference '{}', "
+            "should be 20".format(reference))
+
+    def controlCode(string):
+        posweight=[13,15,12,5,4,17,9,21,3,7,1]
+        dcletter='MQWERTYUIOPASDFGHJKLBZX'
+        dc=0
+        for i,c in enumerate(string):
+            try:
+                charvalue = int(c)
+            except ValueError:
+                charvalue = _valids.index(c)+1
+            dc += posweight[i]*charvalue
+        dc %= 23
+        return dcletter[dc]
+
+    dc1 = controlCode(reference[0:7] + reference[14:18])
+    dc2 = controlCode(reference[7:14] + reference[14:18])
+
+    if dc1+dc2 != reference[18:]:
+        raise ValidationError("Control code should be {} instead of {}"
+            .format(dc1+dc2, reference[18:]))
+
+    return reference
+
+def is_valid(number):
+    """Checks to see if the number provided is a valid Cadastral Reference."""
+    try:
+        return bool(validate(number))
+    except ValidationError:
+        return False
+
+
