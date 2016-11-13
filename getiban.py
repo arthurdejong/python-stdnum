458c310d3bcc0dca4d4db776d3950757ff2b2c76
Arthur de Jong <arthur@arthurdejong.org>
2016-11-13 22:51:57 +0100

    Update gettin IBAN registry
    The format of the registry file has changed. Before it was a
    straightforward CSV file with countries in rows but countries are now in
    columns.

--- getiban.py
+++ getiban.py
@@ -23,54 +23,56 @@
 Financial Telecommunication which is the official IBAN registrar) to get
 the data needed to correctly parse and validate IBANs."""
 
+from collections import defaultdict
 import csv
 import urllib
 
 
-# The place where the current version of IBAN_Registry.txt can be downloaded.
+# The place where the current version of
+# swift_standards_infopaper_ibanregistry_1.txt can be downloaded.
 download_url = 'https://www.swift.com/node/11971'
-
-
-def clean_row(row):
-    """Clean up a read row from the CSV file."""
-    stripit = ' \t\n\r;:\'"'
-    return dict(
-        (k.strip(stripit).lower(), v.strip(stripit))
-        for k, v in row.items())
 
 
 def get_country_codes(line):
     """Return the list of country codes this line has."""
     # simplest case first
-    if len(line['country code as defined in iso 3166']) == 2:
-        return [line['country code as defined in iso 3166']]
+    if len(line['IBAN prefix country code (ISO 3166)']) == 2:
+        return [line['IBAN prefix country code (ISO 3166)']]
     # fall back to parsing the IBAN structure
     return [x.strip()[:2] for x in line['iban structure'].split(',')]
 
 
 def parse(f):
     """Parse the specified file."""
-    print '# generated from IBAN_Registry.txt, downloaded from'
-    print '# %s' % download_url
-    for row in csv.DictReader(f, delimiter='\t', quotechar='"'):
-        row = clean_row(row)
-        bban = row['bban structure']
-        if not(bban) or bban.lower() == 'not in use':
-            bban = row['iban structure']
-        for cc in get_country_codes(row):
-            if bban.startswith(cc + '2!n'):
-                bban = bban[5:]
-            # print country line
-            print '%s country="%s" bban="%s"' % (
-                cc, row['name of country'], bban.replace(' ', ''))
-            # TODO: some countries have a fixed check digit value
-            # TODO: some countries have extra check digits
-            # TODO: use "Bank identifier position within the BBAN" field
-            #       to add labels to the ranges (Bank identifier and Branch
-            #       Identifier)
+    print '# generated from swift_standards_infopaper_ibanregistry_1.txt,'
+    print '# downloaded from %s' % download_url
+    values = defaultdict(dict)
+    # the file is CSV but the data is in columns instead of rows
+    for row in csv.reader(f, delimiter='\t', quotechar='"'):
+        # skip first row
+        if row[0] != 'Data element':
+            # first column contains label
+            for i, c in enumerate(row[1:]):
+                values[i][row[0]] = c
+    # output the collected data
+    for i, data in values.items():
+        bban = data['BBAN structure']
+        if not(bban) or bban.lower() == 'n/a':
+            bban = data['IBAN structure']
+        bban = bban.replace(' ', '')
+        cc = data['IBAN prefix country code (ISO 3166)'][:2]
+        cname = data['Name of country']
+        if bban.startswith(cc + '2!n'):
+            bban = bban[5:]
+        # print country line
+        print '%s country="%s" bban="%s"' % (cc, cname, bban)
+        # TODO: some countries have a fixed check digit value
+        # TODO: some countries have extra check digits
+        # TODO: use "Bank identifier position within the BBAN" field
+        #       to add labels to the ranges (Bank identifier and Branch
+        #       Identifier)
 
 
 if __name__ == '__main__':
-    #f = open('IBAN_Registry.txt', 'r')
     f = urllib.urlopen(download_url)
     parse(f)
