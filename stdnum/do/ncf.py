cce9e4dc082204d0a442ac6e463d9cb13136be9d
Jeffry Jesus De La Rosa <jeffryjesus@gmail.com>
2019-10-11 12:58:27 -0400

    Changing DGII URL and Handle validation data
    DGII changed the __EVENTVALIDATION and __VIEWSTATE in certain, use BeautifulSoup to get the actual value and then make the post.

--- stdnum/do/ncf.py
+++ stdnum/do/ncf.py
@@ -1,4 +1,4 @@
-# ncf.py - functions for handling Dominican Republic invoice numbers
+## ncf.py - functions for handling Dominican Republic invoice numbers
 # coding: utf-8
 #
 # Copyright (C) 2017-2018 Arthur de Jong
@@ -22,30 +22,16 @@
 
 """NCF (Números de Comprobante Fiscal, Dominican Republic receipt number).
 
-The NCF is used to number invoices and other documents for the purpose of tax
-filing. The e-CF (Comprobante Fiscal Electrónico) is used together with a
-digital certificate for the same purpose. The number is either 19, 11 or 13
-(e-CF) digits long.
-
-The 19 digit number starts wit a letter (A or P) to indicate that the number
-was assigned by the taxpayer or the DGII, followed a 2-digit business unit
-number, a 3-digit location number, a 3-digit mechanism identifier, a 2-digit
-document type and a 8-digit serial number.
-
-The 11 digit number always starts with a B followed a 2-digit document type
-and a 7-digit serial number.
-
-The 13 digit e-CF starts with an E followed a 2-digit document type and an
-8-digit serial number.
+The NCF is used to number invoices and other documents for the purposes of
+tax filing. The number is 19 digits long and consists of a letter (A or P) to
+indicate that the number was assigned by the taxpayer or the DGIT, followed a
+2-digit business unit number, a 3-digit location number, a 3-digit mechanism
+identifier, a 2-digit document type and a 8-digit serial number.
 
 More information:
 
  * https://www.dgii.gov.do/
- * https://dgii.gov.do/legislacion/normas/Documents/Norma05-19.pdf
- * https://dgii.gov.do/contribuyentes/personasFisicas/inicioOperaciones/ComprobantesFiscales/Paginas/comprobantesFiscalesElectronicos.aspx
 
->>> validate('E310000000005')  # format since 2019-04-08
-'E310000000005'
 >>> validate('B0100000005')  # format since 2018-05-01
 'B0100000005'
 >>> validate('A020010210100000005')  # format before 2018-05-01
@@ -57,7 +43,7 @@ InvalidFormat: ...
 """
 
 from stdnum.exceptions import *
-from stdnum.util import clean, isdigits
+from stdnum.util import clean
 
 
 def compact(number):
@@ -67,50 +53,30 @@ def compact(number):
 
 
 # The following document types are known:
-_ncf_document_types = (
-    '01',  # invoices for fiscal declaration (or tax reporting)
-    '02',  # invoices for final consumer
-    '03',  # debit note
-    '04',  # credit note (refunds)
-    '11',  # informal supplier invoices (purchases)
-    '12',  # single income record
-    '13',  # minor expenses invoices (purchases)
-    '14',  # invoices for special customers (tourists, free zones)
-    '15',  # invoices for the government
-)
-
-_ecf_document_types = (
-    '31',  # invoices for fiscal declaration (or tax reporting)
-    '32',  # invoices for final consumer
-    '33',  # debit note
-    '34',  # credit note (refunds)
-    '41',  # supplier invoices (purchases)
-    '43',  # minor expenses invoices (purchases)
-    '44',  # invoices for special customers (tourists, free zones)
-    '45',  # invoices for the government
-)
-
+#  01 invoices for fiscal declaration (or tax reporting)
+#  02 invoices for final consumer
+#  03 debit note
+#  04 credit note (refunds)
+#  11 informal supplier invoices (purchases)
+#  12 single income record
+#  13 minor expenses invoices (purchases)
+#  14 invoices for special customers (tourists, free zones)
+#  15 invoices for the government
 
 def validate(number):
     """Check if the number provided is a valid NCF."""
     number = compact(number)
-    if len(number) == 13:
-        if number[0] != 'E' or not isdigits(number[1:]):
+    if len(number) == 11:
+        if number[0] != 'B' or not number[1:].isdigit():
             raise InvalidFormat()
-        if number[1:3] not in _ecf_document_types:
-            raise InvalidComponent()
-    elif len(number) == 11:
-        if number[0] != 'B' or not isdigits(number[1:]):
-            raise InvalidFormat()
-        if number[1:3] not in _ncf_document_types:
-            raise InvalidComponent()
     elif len(number) == 19:
-        if number[0] not in 'AP' or not isdigits(number[1:]):
+        if number[0] not in 'AP' or not number[1:].isdigit():
             raise InvalidFormat()
-        if number[9:11] not in _ncf_document_types:
-            raise InvalidComponent()
     else:
         raise InvalidLength()
+    if number[-10:-8] not in (
+            '01', '02', '03', '04', '11', '12', '13', '14', '15'):
+        raise InvalidComponent()
     return number
 
 
@@ -169,19 +135,24 @@ def check_dgii(rnc, ncf, timeout=30):  # pragma: no cover
     from stdnum.do.rnc import compact as rnc_compact
     rnc = rnc_compact(rnc)
     ncf = compact(ncf)
-    url = 'https://www.dgii.gov.do/app/WebApps/ConsultasWeb/consultas/ncf.aspx'
+    url = 'https://dgii.gov.do/app/WebApps/ConsultasWeb2/ConsultasWeb/consultas/ncf.aspx'
     headers = {
         'User-Agent': 'Mozilla/5.0 (python-stdnum)',
     }
+
+    html_content = requests.get(url).text
+    validation = BeautifulSoup(html_content, "lxml").find('input', {'name':'__EVENTVALIDATION'})['value']
+    view = BeautifulSoup(html_content, "lxml").find('input', {'name': '__VIEWSTATE'})['value']
+
     data = {
-        '__EVENTVALIDATION': '/wEWBAKh8pDuCgK+9LSUBQLfnOXIDAKErv7SBhjZB34//pbvvJzrbkFCGGPRElcd',
-        '__VIEWSTATE': '/wEPDwUJNTM1NDc0MDQ5ZGRCFUYoDcVRgzEntcKfSuvPnC2VhA==',
-        'ctl00$cphMain$btnConsultar': 'Consultar',
+        '__EVENTVALIDATION': validation,
+        '__VIEWSTATE': view,
+        'ctl00$cphMain$btnConsultar': 'Buscar',
         'ctl00$cphMain$txtNCF': ncf,
         'ctl00$cphMain$txtRNC': rnc,
     }
     result = BeautifulSoup(
-        requests.post(url, headers=headers, data=data, timeout=timeout).text)
+        requests.post(url, headers=headers, data=data, timeout=timeout).text, features = "lxml")
     results = result.find(id='ctl00_cphMain_pResultado')
     if results:
         data = {
