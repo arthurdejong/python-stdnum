40ac6c028a2ecd2ebc940e421340c322cfe344be
Nicolas Martinelli <nim@odoo.com>
2020-01-09 10:21:05 +0100

    New BTW identification number (NL)
    As of January 1st 2020, a new BTW identification number has been
    introduced in Netherlands.
    
    This commit implements the new check algorithm.
    
    The algorithm detail can be found in:
    http://kleineondernemer.nl/index.php/nieuw-btw-identificatienummer-vanaf-1-januari-2020-voor-eenmanszaken
    
    Closes #182

--- stdnum/nl/btw.py
+++ stdnum/nl/btw.py
@@ -32,15 +32,31 @@ More information:
 '004495445B01'
 >>> validate('NL4495445B01')
 '004495445B01'
+>>> validate('NL002455799B11')
+'002455799B11'
 >>> validate('123456789B90')
 Traceback (most recent call last):
     ...
 InvalidChecksum: ...
 """
 
+import re
+import string
+
 from stdnum.exceptions import *
 from stdnum.nl import bsn
 from stdnum.util import clean, isdigits
+
+
+# regular expression for matching number
+_btw_re = re.compile(r'(?:NL)?[0-9A-Z+*]{10}[0-9]{2}')
+
+# Match letters to integers
+_char_to_int = {}
+for k in string.ascii_uppercase:
+    _char_to_int[k] = str(ord(k) - 55)
+_char_to_int['+'] = '36'
+_char_to_int['*'] = '37'
 
 
 def compact(number):
@@ -53,16 +69,27 @@ def compact(number):
 
 
 def validate(number):
-    """Check if the number is a valid BTW number. This checks the length,
-    formatting and check digit."""
+    """Check if the number is a valid BTW number. Two possible checks:
+    - For natural persons
+    - For non-natural persons and combinations of natural persons (company)"""
     number = compact(number)
-    if not isdigits(number[10:]) or int(number[10:]) <= 0:
-        raise InvalidFormat()
     if len(number) != 12:
         raise InvalidLength()
-    if number[9] != 'B':
+    if not _btw_re.match(number):
         raise InvalidFormat()
-    bsn.validate(number[:9])
+
+    # Natural person => mod97 full checksum
+    check_val_natural = '2321'
+    for x in number:
+        check_val_natural += x if isdigits(x) else _char_to_int[x]
+    if int(check_val_natural) % 97 == 1:
+        return number
+
+    # Company => weighted(9->2) mod11 on bsn
+    if isdigits(number[:9]) and number[9] == 'B' and int(number[10:]) > 0:
+        bsn.validate(number[:9])
+    else:
+        raise InvalidFormat()
     return number
 
 
