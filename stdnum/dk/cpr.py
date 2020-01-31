4500881cf6aad1ddd1ad8b3bcc1495f5a48c45cf
Leon Sandøy <leon.haland@gmail.com>
2020-01-31 17:55:09 +0100

    Improve birth date validation for dk.cpr
    This adds a check to validate that the birth date information in a
    personnummer is not set in the future, resolving a TODO from this file.
    
    It also improves exception messages for certain validation fail
    conditions.

--- stdnum/dk/cpr.py
+++ stdnum/dk/cpr.py
@@ -1,6 +1,8 @@
 # cpr.py - functions for handling Danish CPR numbers
+# coding: utf-8
 #
 # Copyright (C) 2012-2019 Arthur de Jong
+# Copyright (C) 2020 Leon Sandøy
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -44,10 +46,12 @@ More information:
 Traceback (most recent call last):
     ...
 InvalidComponent: ...
+>>> validate('2110525629')
+Traceback (most recent call last):
+  ...
+InvalidComponent: The birth date information is valid, but this person has not been born yet.
 >>> get_birth_date('2110620629')
 datetime.date(1962, 10, 21)
->>> get_birth_date('2110525629')
-datetime.date(2052, 10, 21)
 >>> format('2110625629')
 '211062-5629'
 """
@@ -86,7 +90,7 @@ def get_birth_date(number):
     try:
         return datetime.date(year, month, day)
     except ValueError:
-        raise InvalidComponent()
+        raise InvalidComponent('The number does not contain valid birth date information.')
 
 
 def validate(number):
@@ -97,9 +101,9 @@ def validate(number):
         raise InvalidFormat()
     if len(number) != 10:
         raise InvalidLength()
-    # check if birth date is valid
-    get_birth_date(number)
-    # TODO: check that the birth date is not in the future
+    if get_birth_date(number) > datetime.date.today():
+        raise InvalidComponent(
+            'The birth date information is valid, but this person has not been born yet.')
     return number
 
 
