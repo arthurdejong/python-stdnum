992dc2087f54274049a166ac87472e0b66450813
Arthur de Jong <arthur@arthurdejong.org>
2020-09-04 14:58:50 +0200

    Upgrade jQuery to 3.5.1
    This also runs eslint over check.js and fixes an issue from the upgrade.

--- online_check/check.js
+++ online_check/check.js
@@ -1,7 +1,7 @@
 /*
  # check.js - simple application to check numbers
  #
- # Copyright (C) 2017-2018 Arthur de Jong.
+ # Copyright (C) 2017-2020 Arthur de Jong.
  #
  # This library is free software; you can redistribute it and/or
  # modify it under the terms of the GNU Lesser General Public
@@ -19,13 +19,12 @@
  # 02110-1301 USA
  */
 
-$( document ).ready(function() {
-
+$(document).ready(function () {
   function format(value) {
-    return $("<div/>").text(value).html().replace(
-      /\n\n/g, "<br/>\n"
+    return $('<div/>').text(value).html().replace(
+      /\n\n/g, '<br/>\n'
     ).replace(
-      /^[*] (.*)$/gm, "<ul><li>$1</li></ul>"
+      /^[*] (.*)$/gm, '<ul><li>$1</li></ul>'
     ).replace(
       /(\b(https?|ftp):\/\/[^\s<]*[-\w+&@#/%=~_|])/ig,
       "<a href='$1'>$1</a>"
@@ -34,98 +33,96 @@ $( document ).ready(function() {
 
   function updateresults(field, results) {
     // build HTML to present
-    var h = ["<ul>"];
-    $.each(results, function(index, result) {
+    var h = ['<ul>']
+    $.each(results, function (index, result) {
       h.push(
-        "<li>",
-        $("<div/>").text(result["number"]).html(),
-        ": <b>",
-        $("<div/>").text(result["name"]).html(),
-        "</b>",
-        "<p>",
-        format(result["description"]),
-        $.map(result["conversions"], function(value, key){
+        '<li>',
+        $('<div/>').text(result.number).html(),
+        ': <b>',
+        $('<div/>').text(result.name).html(),
+        '</b>',
+        '<p>',
+        format(result.description),
+        $.map(result.conversions, function (value, key) {
           return [
-            "<br/><b><i>",
-            $("<div/>").text(key).html(),
-            "</i></b>: ",
-            $("<div/>").text(value).html()].join('')
+            '<br/><b><i>',
+            $('<div/>').text(key).html(),
+            '</i></b>: ',
+            $('<div/>').text(value).html()].join('')
         }).join(''),
-        "</p></li>")
-    });
-    h.push("</ul>");
+        '</p></li>')
+    })
+    h.push('</ul>')
     // replace the results div
-    $("#" + $(field).attr("id") + "_results").html(h.join(""));
+    $('#' + $(field).attr('id') + '_results').html(h.join(''))
   }
 
   function checkfield(field) {
-    var value = field.val();
+    var value = field.val()
     // only trigger update if value changed from previous validation
-    if (value != field.data("oldvalue")) {
-      field.data("oldvalue", value);
-      $("#" + $(field).attr("id") + "_results").slideUp(200, function() {
-        $.get('', {"number": value}, function(data) {
-          window.history.pushState({"value": value, "data": data}, $(document).find("title").text(), "?number=" + encodeURIComponent(value));
-          updateresults(field, data);
-        });
-        $(this).slideDown(300);
-      });
+    if (value !== field.data('oldvalue')) {
+      field.data('oldvalue', value)
+      $('#' + $(field).attr('id') + '_results').slideUp(200, function () {
+        $.get('.', {number: value}, function (data) {
+          window.history.pushState({value: value, data: data}, $(document).find('title').text(), '?number=' + encodeURIComponent(value))
+          updateresults(field, data)
+        })
+        $(this).slideDown(300)
+      })
     }
   }
 
   // update results based on history navigation
-  window.onpopstate = function(e) {
-    var field = $(".stdnum_check");
+  window.onpopstate = function (e) {
+    var field = $('.stdnum_check')
     if (e.state) {
-      var value = e.state.value;
-      var data = e.state.data;
+      var value = e.state.value
+      var data = e.state.data
       field.val(value)
-      field.data("oldvalue", value);
-      updateresults(field, data);
+      field.data('oldvalue', value)
+      updateresults(field, data)
     } else {
-      field.val("")
-      field.data("oldvalue", "");
-      updateresults(field, []);
+      field.val('')
+      field.data('oldvalue', '')
+      updateresults(field, [])
     }
-  };
+  }
 
   // trigger a check when user stopped typing
-  $(".stdnum_check").on("input propertychange", function (event) {
-      if (window.event && event.type == "propertychange" && event.propertyName != "value")
-        return;
-      var field = $(this);
-      window.clearTimeout($(this).data("timeout"));
-      $(this).data("timeout", setTimeout(function () {
-        checkfield(field);
-    }, 2000));
-  });
-
-  // trigger a check when losing focus
-  $(".stdnum_check").on("blur", function() {
-    window.clearTimeout($(this).data("timeout"));
-    checkfield($(this));
-  });
-
-  // prevent enter from submitting the form
-  $(".stdnum_check").keydown(function(event) {
-    if(event.keyCode == 13) {
-      event.preventDefault();
-      checkfield($(this));
-      return false;
-    }
-  });
-
-  // hide the submit button
-  $(".stdnum_hide").hide();
-
-  // focus the text field
-  $(".stdnum_check").focus();
-
-  // save current state
-  var value = $(".stdnum_check").val();
-  $(".stdnum_check").data("oldvalue", value);
-  $.get('', {number: value}, function(data) {
-    window.history.replaceState({"value": value, "data": data}, $(document).find("title").text(), "?number=" + encodeURIComponent(value));
+  $('.stdnum_check').on('input propertychange', function (event) {
+    if (window.event && event.type === 'propertychange' && event.propertyName !== 'value') { return }
+    var field = $(this)
+    window.clearTimeout($(this).data('timeout'))
+    $(this).data('timeout', setTimeout(function () {
+      checkfield(field)
+    }, 2000))
   })
 
-});
+  // trigger a check when losing focus
+  $('.stdnum_check').on('blur', function () {
+    window.clearTimeout($(this).data('timeout'))
+    checkfield($(this))
+  })
+
+  // prevent enter from submitting the form
+  $('.stdnum_check').keydown(function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault()
+      checkfield($(this))
+      return false
+    }
+  })
+
+  // hide the submit button
+  $('.stdnum_hide').hide()
+
+  // focus the text field
+  $('.stdnum_check').focus()
+
+  // save current state
+  var value = $('.stdnum_check').val()
+  $('.stdnum_check').data('oldvalue', value)
+  $.get('', {number: value}, function (data) {
+    window.history.replaceState({value: value, data: data}, $(document).find('title').text(), '?number=' + encodeURIComponent(value))
+  })
+})
