e951daca447f678f5a3f3c70dcc535eb8d7449b4
Arthur de Jong <arthur@arthurdejong.org>
2024-06-23 16:12:59 +0200

    Support 16 digit Indonesian NPWP numbers
    The Indonesian NPWP is being switched from 15 to 16 digits. The number
    is now the NIK for Indonesian citizens and the old format with a leading
    0 for others (organisations and non-citizens).
    
    See https://www.grantthornton.co.id/insights/global-insights1/updates-regarding-the-format-of-indonesian-tax-id-numbers/
    
    Closes https://github.com/arthurdejong/python-stdnum/issues/432

--- stdnum/id/npwp.py
+++ stdnum/id/npwp.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 #
 # Copyright (C) 2020 Leandro Regueiro
+# Copyright (C) 2024 Arthur de Jong
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -24,10 +25,14 @@ The Nomor Pokok Wajib Pajak (NPWP) is assigned to organisations and
 individuals (families) by the Indonesian Tax Office after registration by the
 tax payers.
 
-The number consists of 15 digits of which the first 2 denote the type of
+The number consists of 16 digits and is either a NIK (Nomor Induk Kependudukan)
+or a number that starts with a 0, followed by 2 digits that denote the type of
 entity, 6 digits to identify the tax payer, a check digit over the first 8
 digits followed by 3 digits to identify the local tax office and 3 digits for
 branch code.
+
+This module also accepts the old 15 digit format which is just the 16 digit
+format without the starting 0.
 
 More information:
 
@@ -49,6 +54,7 @@ InvalidLength: ...
 
 from stdnum import luhn
 from stdnum.exceptions import *
+from stdnum.id import nik
 from stdnum.util import clean, isdigits
 
 
@@ -64,12 +70,20 @@ def compact(number):
 def validate(number):
     """Check if the number is a valid Indonesia NPWP number."""
     number = compact(number)
-    if len(number) != 15:
-        raise InvalidLength()
     if not isdigits(number):
         raise InvalidFormat()
-    luhn.validate(number[:9])
-    return number
+    if len(number) == 15:
+        # Old 15 digit format
+        luhn.validate(number[:9])
+        return number
+    if len(number) == 16:
+        # New format since 2024: either a NIK (for Indonesian citizens) or
+        # the old number with a 0 at the beginning
+        if not number.startswith('0'):
+            return nik.validate(number)
+        luhn.validate(number[:10])
+        return number
+    raise InvalidLength()
 
 
 def is_valid(number):
