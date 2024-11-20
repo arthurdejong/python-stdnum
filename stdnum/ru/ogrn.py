c237cfcbc2f2424bea5e22e6131cfa45dac59d21
nvmbrasserie <ivan@opensanctions.org>
2024-11-20 18:08:50 +0100

    tests successful

--- stdnum/ru/ogrn.py
+++ stdnum/ru/ogrn.py
@@ -25,43 +25,48 @@ The OGRN is a Russian  identifier for legal entities that consists of either 13 
 
 >>> validate('1022200525819')
 True
->>> validate('1027739') # too short
-Traceback (most recent call last):
-    ...
 >>> validate('1022500001325')
 True
 >>> validate('10277395526422') # 14 digits
 Traceback (most recent call last):
     ...
+InvalidLength: ...
 >>> validate('1022500001328') # invalid check digit
 Traceback (most recent call last):
     ...
+InvalidChecksum: ...
 """
 
 import re
 from typing import Optional
 
-from stdnum.exceptions import ValidationError
+from stdnum.exceptions import ValidationError, InvalidLength, InvalidChecksum, InvalidComponent
 
 
-def is_valid(text: str):
+def validate(text: str) -> bool:
     """Determine if the given string is a valid OGRN."""
-    if re.compile(r'\b(\d{13}|\d{15})\b').match(text) is None:
-        raise ValidationError('Invalid length for OGRN.')
+    if not isinstance(text, str) or text is None:
+        raise TypeError("Expected a string or bytes-like object, got '{}'"
+                        .format(type(text).__name__))
+    if len(text) not in {13, 15} or not text.isdigit():
+        raise InvalidLength()
     if text[0] == '0':
-        raise ValidationError('Invalid first digit for OGRN.')
+        raise InvalidComponent()
     federal_subject_code = int(text[3:5])
     if federal_subject_code not in set(range(1, 80)) | {83, 86, 87, 89, 91, 92, 99}:
-        raise ValidationError('Invalid check digit for OGRN.')
+        raise InvalidComponent()
     control_digit = int(text[-1])
-    return control_digit == calculate_control_digit(text)
-
+    if control_digit != calculate_control_digit(text):
+        raise InvalidChecksum()
+    return True
 
 def format(text: str) -> Optional[str]:
     """Normalize the given string to a valid OGRN."""
+    if not isinstance(text, str):
+        return None
     match = re.compile(r'\b(\d{13}|\d{15})\b').search(text)
     if match is None:
-        return None
+        raise InvalidLength()
     return match.group(1)
 
 
@@ -80,9 +85,10 @@ def calculate_control_digit(grn: str) -> Optional[int]:
     return None
 
 
-def validate(text: str) -> bool:
+def is_valid(text: str) -> bool:
     """Check if the number is a valid OGRN."""
-    normalized_text = format(text)
-    if normalized_text is None:
+    try:    
+        normalized_text = format(text)
+        return bool(normalized_text and validate(normalized_text))
+    except ValidationError:
         return False
-    return is_valid(normalized_text)
